b38ff978abd802bc094ebdf0096568dcc3f1e4b5
Nathan Heseman <nheseman@outlook.com>
2019-01-27 18:37:22 -0600

    Login update

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer text-center">
+        All Rights Reserved by Starfleet Service Dogs, Inc. &copy;2019
+      </footer>
+    );
+  }
+}
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import { createBrowserHistory } from 'history';
+import indexRoutes from './routes/index.jsx';
+import { Route, Switch } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
+import './assets/scss/style.css';
+
+//const hist = createBrowserHistory();
+
+ReactDOM.render(
+ 
+  <HashRouter>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
+      })}
+    </Switch>
+  </HashRouter>
+  ,document.getElementById('root')); 
